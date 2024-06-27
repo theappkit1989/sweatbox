b8451582e52fb64d3c64003404c23569f651d6cd
Codecoy <muhammadyasirxeikh@gmail.com>
2024-06-27 11:47:26 +0500

    push update 27-06

--- lib/modules/massage_booking/testpaymentService/payment_config.dart
+++ lib/modules/massage_booking/testpaymentService/payment_config.dart
@@ -73,7 +73,7 @@ const String defaultGooglePay = '''{
         "tokenizationSpecification": {
           "type": "PAYMENT_GATEWAY",
           "parameters": {
-            "gateway": "paynt",
+            "gateway": "example",
             "gatewayMerchantId": "8ac7a4c88feb836d018fed0b84160266"
           }
         },
